74c2f88f176ea03d34285730fd24f862f575ed9c
Marcos Puglisi <profe.puglisi@gmail.com>
2021-07-29 14:12:00 -0300

    error 401, auth y home

--- routes/admin/usuarios.js
+++ routes/admin/usuarios.js
@@ -1,11 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const model = require('./../../models/usuarios');
 
-const get = async(req,res) => {
-    const usuarios = await model.getAll();
-    res.render('adminUsuarios' , {usuarios});
-}
-
-router.get('/', get);
 module.exports = router;
