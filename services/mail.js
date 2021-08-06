99676135bf3dbb2a824fa6521610ac15cf35e52d
Marcos Puglisi <profe.puglisi@gmail.com>
2021-08-06 17:28:03 -0300

    ültima modificacion

--- services/mail.js
+++ services/mail.js
@@ -1,4 +1,4 @@
- 
+ /*
    const nodemailer = require('nodemailer');
     // envio de mail tiene asociada una demora entonces es async await
 
@@ -7,7 +7,7 @@
         const transporter = nodemailer.createTransport({
             //host: 'smtp.gmail.com',
             //port: 587,
-            secure: false ,
+            //secure: false ,
             //es lo mismo que le service gmail
             service : process.env.MAIL_SERVICE,//|| 'gmail',
             auth : {
@@ -31,7 +31,7 @@
     module.exports = {send};
 
 
-
+*/
 
 
 
@@ -61,3 +61,34 @@ const send = async({mail, asunto = 'Gracias por registrarse', cuerpo}) => {
 }
 
 module.exports = {send};*/
+
+const nodemailer = require('nodemailer');
+// envio de mail tiene asociada una demora entonces es async await
+
+const send = async({mail, asunto = 'Gracias por registrarte :D!!', cuerpo}) => {
+    try{
+    const transporter = nodemailer.createTransport({
+        /*host: 'smtp.gmail.com',
+        port: 587,
+        secure: false 
+        es lo mismo que le service gmail*/
+        service : process.env.MAIL_SERVICE || 'gmail',
+        auth : {
+            user: process.env.MAIL_USER || 'mail',
+            pass: process.env.MAIL_PASS || 'password'
+        }
+    });
+    const info = {
+        to: mail,
+        subject: asunto,
+        html: cuerpo
+    };
+    const {messageId} = await transporter.sendMail(info);
+    return messageId;
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+module.exports = {send};
